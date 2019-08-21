8565bef93e56a90b78bff265a07a4990fb3566a2
Kihong Heo <kheo@cis.upenn.edu>
2019-08-21 12:37:56 -0400

    delete old code

--- static/test/input0.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  int x = fgetc(stdin);
-  int y = 5 / x;
-  return 0;
-}
--- static/test/pointer0.c
+++
@@ -1,8 +0,0 @@
-int f(){
-  int a;
-  int *c = &a;
-  int *d = &a;
-  *c = 0;
-  int x = 1 / *d;
-  return 0;
-}
